aaf4db3885b0a45922cdb7c94c7f30f49e84d43b
UOC\2407112 <harrisonfwoollard@gmail.com>
2025-01-14 11:19:35 +0000

    Added some waypoints and a script with non-working code from a previouse project, I shan't be using that code to get the enemies movement working

---
+++ Assets/Scripts/BluePath.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BluePath : MonoBehaviour
+{
+
+    /*[SerializeField] GameObject enemyPrefab;
+    [SerializeField] GameObject pathPrefab;
+
+
+
+
+
+   
+
+
+
+
+
+    //params
+    int waypointIndex = 0;
+    [SerializeField] float enemyMoveSpeed = 2f;
+
+    //cached refs
+    List<Transform> waypoints;
+   
+
+    private void Start()
+    {
+        // waypoint = BluePath.GetWayPoints();
+
+        public List<Transform> GetWayPoints()
+        {
+            var waveWaypoints = new List<Transform>();
+
+            foreach (Transform waypoint in pathPrefab.transform)
+            {
+                waveWaypoints.Add(waypoint);
+            }
+
+            return waveWaypoints;
+        }
+
+
+
+
+
+    }
+
+    private void Update()
+    {
+        MoveEnemyOnPath();
+    }
+
+    private void MoveEnemyOnPath()
+    {
+        if (waypointIndex <= waypoints.Count - 1)
+        {
+            var targetWaypointPosition = waypoints[waypointIndex].transform.position;
+            transform.position = Vector2.MoveTowards(transform.position, targetWaypointPosition, enemyMoveSpeed * Time.deltaTime);
+
+            if (transform.position == targetWaypointPosition)
+            {
+                waypointIndex++;
+            }
+        }
+
+       
+    }
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
